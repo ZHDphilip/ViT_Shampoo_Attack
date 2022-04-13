67e2d15f104c74482db96376d163711c1e3b49ba
ZHDphilip <zihaodong@ucla.edu>
2022-04-12 20:05:36 -0700

    dim fix

--- attach_ViT.py
+++ attach_ViT.py
@@ -3,6 +3,34 @@ from shampoo_attack_ViT import ShampooAttackViT
 import torch
 import torch.nn as nn
 
+from PIL import Image
+import timm
+import torchvision.transforms as transforms
+from timm.data.constants import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
+
 def main():
-    
-    atk = ShampooAttackViT()
+    model = torch.hub.load('facebookresearch/deit:main', 'deit_base_patch16_224', pretrained=True)
+    model.eval()
+
+    transform = transforms.Compose([
+        transforms.Resize(256, interpolation=3),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD),
+    ])
+
+    img = Image.open("../test.jpeg")
+    inputs = transform(img)[None, ]
+    stackedinputs = torch.stack([inputs, inputs]).squeeze(dim=1)
+    out = model(stackedinputs)
+    clsidx = torch.argmax(out, dim=1)
+    print(f"original prediction: {clsidx}")
+    atk = ShampooAttackViT(model, 224, 16, epsilon=1)
+    advinputs = atk(stackedinputs, clsidx)
+    out2 = model(advinputs)
+    clsidx2 = torch.argmax(out2, dim=1)
+    print(f"new prediction: {clsidx2}")
+
+
+if __name__ == "__main__":
+    main()
--- shampoo_attack_ViT.py
+++ shampoo_attack_ViT.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn.ensemble import GradientBoostingClassifier
 import torch
 import torch.nn as nn
 
@@ -8,7 +9,7 @@ def _matrix_power(matrix, power):
     # use CPU for svd for speed up
     matrix = matrix.cpu()
     u, s, v = torch.svd(matrix)
-    return (u @ s.pow_(power).diag() @ v.t()).cuda()
+    return (u @ s.pow_(power).diag() @ v.t())#.cuda()
 
 class ShampooAttackViT(Attack):
     '''
@@ -28,7 +29,7 @@ class ShampooAttackViT(Attack):
         self._supported_mode = ['default', 'targeted']
         self.image_dim = image_dim
         self.patch_sidelen = patch_sidelen
-        self.patch_num_side = self.image_dim / self.patch_sidelen
+        self.patch_num_side = int(self.image_dim / self.patch_sidelen)
         # in total self.patch_num_side ** 2 patches
     
 
@@ -43,6 +44,7 @@ class ShampooAttackViT(Attack):
 
         images.requires_grad = True
         outputs = self.model(images)
+        #outlabels = torch.argmax(outputs)
 
         if self._targeted:
             cost = -loss(outputs, target_labels)
@@ -58,23 +60,46 @@ class ShampooAttackViT(Attack):
         #   and thus n sets of L and R preconditioners
 
         # initializing precondition matrices
-        preconds = torch.zeros(images.shape[0], self.patch_num_side, self.patch_num_side, 2, self.patch_sidelen, self.patch_sidelen)
-        for i in images.shape[0]:
+        preconds = torch.zeros(images.shape[0], self.patch_num_side, self.patch_num_side, 6, self.patch_sidelen, self.patch_sidelen)
+        for i in range(images.shape[0]):
             for j in range(self.patch_num_side):
                 for k in range(self.patch_num_side):
-                    preconds[i][j][k][0] = self.epsilon * torch.eye(self.patch_sidelen)    # initialize L
-                    preconds[i][j][k][1] = self.epsilon * torch.eye(self.patch_sidelen)    # initialize R
+                    preconds[i][j][k][0] = self.epsilon * torch.eye(self.patch_sidelen)    # initialize L Red
+                    preconds[i][j][k][1] = self.epsilon * torch.eye(self.patch_sidelen)    # initialize L Green
+                    preconds[i][j][k][2] = self.epsilon * torch.eye(self.patch_sidelen)    # initialize L Blue
+                    preconds[i][j][k][3] = self.epsilon * torch.eye(self.patch_sidelen)    # initialize R Red
+                    preconds[i][j][k][4] = self.epsilon * torch.eye(self.patch_sidelen)    # initialize R Green
+                    preconds[i][j][k][5] = self.epsilon * torch.eye(self.patch_sidelen)    # initialize R Blue
         
         advimages = images.clone().detach().to(self.device)
+        print(advimages.shape)
 
-        for i in images.shape[0]:
+        print(f"grad shape:{grad.shape}")
+        grad = grad.permute(0, 2, 3, 1)
+        print(f"grad shape permuted:{grad.shape}")
+
+        for i in range(images.shape[0]):
             for j in range(self.patch_num_side):
                 for k in range(self.patch_num_side):
-                    gradient_block = grad[i][(j-1)*self.patch_sidelen:j*self.patch_sidelen][(k-1)*self.patch_sidelen:k*self.patch_sidelen]
-                    preconds[i][j][k][0] = preconds[i][j][k][0] + gradient_block @ gradient_block.t()
-                    preconds[i][j][k][1] = preconds[i][j][k][1] + gradient_block.t() @ gradient_block
-                    advimages[i][(j-1)*self.patch_sidelen:j*self.patch_sidelen][(k-1)*self.patch_sidelen:k*self.patch_sidelen] = \
-                        advimages[i][(j-1)*self.patch_sidelen:j*self.patch_sidelen][(k-1)*self.patch_sidelen:k*self.patch_sidelen] \
-                        + self.epsilon * _matrix_power(preconds[i][j][k][0], -1/4) @ gradient_block @ _matrix_power(preconds[i][j][k][1], -1/4)
+                    gradient_block = grad[i, j*self.patch_sidelen:(j+1)*self.patch_sidelen, k*self.patch_sidelen:(k+1)*self.patch_sidelen, :]
+                    preconds[i][j][k][0] = preconds[i][j][k][0] + gradient_block[:, :, 0] @ gradient_block[:, :, 0].t()   # update L Red
+                    preconds[i][j][k][1] = preconds[i][j][k][1] + gradient_block[:, :, 1] @ gradient_block[:, :, 1].t()   # update L Green
+                    preconds[i][j][k][2] = preconds[i][j][k][2] + gradient_block[:, :, 2] @ gradient_block[:, :, 2].t()   # update L Blue
+                    preconds[i][j][k][3] = preconds[i][j][k][3] + gradient_block[:, :, 0].t() @ gradient_block[:, :, 0]     # update R Red
+                    preconds[i][j][k][4] = preconds[i][j][k][4] + gradient_block[:, :, 1].t() @ gradient_block[:, :, 1]     # update R Green
+                    preconds[i][j][k][5] = preconds[i][j][k][5] + gradient_block[:, :, 2].t() @ gradient_block[:, :, 2]     # update R Blue
+                    # print(advimages[i, 0, j*self.patch_sidelen:(j+1)*self.patch_sidelen, k*self.patch_sidelen:(k+1)*self.patch_sidelen].shape)
+                    # print((self.epsilon * _matrix_power(preconds[i][j][k][0], -1/4) @ gradient_block[:, :, 0] @ _matrix_power(preconds[i][j][k][3], -1/4)).shape)
+                    advimages[i, 0, j*self.patch_sidelen:(j+1)*self.patch_sidelen, k*self.patch_sidelen:(k+1)*self.patch_sidelen] = \
+                        advimages[i, 0, j*self.patch_sidelen:(j+1)*self.patch_sidelen, k*self.patch_sidelen:(k+1)*self.patch_sidelen] \
+                        + self.epsilon * _matrix_power(preconds[i][j][k][0], -1/4) @ gradient_block[:, :, 0] @ _matrix_power(preconds[i][j][k][3], -1/4)     # update Red Channel
+                    print(advimages[i, 0, j*self.patch_sidelen:(j+1)*self.patch_sidelen, k*self.patch_sidelen:(k+1)*self.patch_sidelen])
+                    print(_matrix_power(preconds[i][j][k][0], -1/4) @ gradient_block[:, :, 0] @ _matrix_power(preconds[i][j][k][3], -1/4))
+                    advimages[i, 1, j*self.patch_sidelen:(j+1)*self.patch_sidelen, k*self.patch_sidelen:(k+1)*self.patch_sidelen] = \
+                        advimages[i, 1, j*self.patch_sidelen:(j+1)*self.patch_sidelen, k*self.patch_sidelen:(k+1)*self.patch_sidelen] \
+                        + self.epsilon * _matrix_power(preconds[i][j][k][1], -1/4) @ gradient_block[:, :, 1] @ _matrix_power(preconds[i][j][k][4], -1/4)     # update Green Channel
+                    advimages[i, 2, j*self.patch_sidelen:(j+1)*self.patch_sidelen, k*self.patch_sidelen:(k+1)*self.patch_sidelen] = \
+                        advimages[i, 2, j*self.patch_sidelen:(j+1)*self.patch_sidelen, k*self.patch_sidelen:(k+1)*self.patch_sidelen] \
+                        + self.epsilon * _matrix_power(preconds[i][j][k][2], -1/4) @ gradient_block[:, :, 2] @ _matrix_power(preconds[i][j][k][5], -1/4)     # update Blue Channel
         
         return advimages
